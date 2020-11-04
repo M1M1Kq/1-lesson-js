505b668687da317f2f381e648fa04e42e6510f8a
M1M1K <neketaseg228@gmail.com>
2020-11-04 11:54:22 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/*jshint esversion: 6 */
+
+
+const temeratureC = parseInt(prompt('Введите температуру в градусах Цельсия: '));
+
+const temeratureF = 9 / 5 * temeratureC + 32;
+
+alert(`Температура в градусах цельсия : ${temeratureF}`);
+
+// Цельсий - Фаренгейт
+
+let name = 'Василий';
+let admin = name;
+
+alert(admin);
+
+// Admin Василий
+
